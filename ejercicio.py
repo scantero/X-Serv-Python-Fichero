9d690e9199bbbb078010cb36cbe48f7254e57596
Sandra Cantero Rabanillo <sdkr@zeta40.aulas.gsyc.es>
2016-02-02 12:47:41 +0100

    He realizado el ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+fichero = open("/etc/passwd", "r")
 
+usuarios = fichero.readlines()
+
+for linea in usuarios:
+    contenido = linea.split(":")
+    print "usuario:", contenido[0], "-> shell:", contenido[-1],
+    
+print "Número de usuarios:",len(usuarios),
+    
+fichero.close()
